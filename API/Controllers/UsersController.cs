fbd55e1da19538589690e1c83aff6ea8aad53e6a
mmendak <mikolajmendak77@gmail.com>
2022-12-02 19:36:27 +0100

    first commit

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,38 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")] //GET   -    user will be using -     /api/users - because of the slash forward
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context) 
+        {
+            _context=context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<AppUser>>GetUsers()
+        {
+            var users = _context.Users.ToList();
+
+            return users;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<AppUser> GetUser(int id) 
+        {
+            var user = _context.Users.Find(id);
+            
+            return user;
+        }
+
+
+
+    }
+}
